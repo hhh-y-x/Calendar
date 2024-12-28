a069ecf6ab7efe9ad34a7c9143ae52a30bd97a4b
Artem Bondar <githublab13@gmail.com>
2024-12-28 16:10:15 -0500

    Adjust weekday logic in getMondayFirst to make Monday the first day of the week

--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -4,4 +4,14 @@ export class CreateCalendar {
     this.year = year;
     this.month = month;
   };
+  
+  getMondayFirst(date) {
+    let weekDay = date;
+    
+    if (weekDay === 0) {
+      weekDay = 7
+    };
+    
+    return weekDay - 1;
+  };
 };
