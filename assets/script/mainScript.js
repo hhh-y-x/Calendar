7084bb7cd247fc0d544b014e30894a71c6aea072
Artem Bondar <githublab13@gmail.com>
2024-12-29 19:18:45 -0500

    Import and activate all listeners for calendar controls in mainScript.js

---
+++ assets/script/mainScript.js
@@ -0,0 +1,7 @@
+import { CreateCalendar } from '../../components/calendar/calendar.js';
+import { activateAllListeners } from '../../components/change/change.js';
+
+const calendarCalendar = new CreateCalendar();
+calendarCalendar.currentCalendar();
+
+activateAllListeners();
