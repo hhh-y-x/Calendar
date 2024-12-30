836c1365d7043215a6b949f3724dae6237c25573
Artem Bondar <githublab13@gmail.com>
2024-12-29 19:02:04 -0500

    Add month array and date initialization for calendar functionality

--- components/change/change.js
+++ components/change/change.js
@@ -1,1 +1,11 @@
 import { CreateCalendar } from '../../components/calendar/calendar.js';
+
+const months = [
+  "January", "February", "March", "April",
+  "May", "June", "July", "August", 
+  "September", "October", "November", "December"
+];
+
+const headerMonth = document.querySelector('.change__month');
+const headerYear = document.querySelector('.change__year');
+const now = new Date();
