c2bc9126497cf47c44b82785d8efee6a8e42a1e9
Artem Bondar <githublab13@gmail.com>
2024-12-31 23:38:18 -0500

    Import and call searchMonthList function for month search functionality

--- assets/script/mainScript.js
+++ assets/script/mainScript.js
@@ -1,7 +1,9 @@
 import { CreateCalendar } from '../../components/calendar/calendar.js';
 import { activateAllListeners } from '../../components/change/change.js';
+import { searchMonthList } from '../../components/input/input__month-search/input__month-search.js';
 
 const calendarCalendar = new CreateCalendar();
 calendarCalendar.currentCalendar();
 
 activateAllListeners();
+searchMonthList();
