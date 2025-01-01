5cd30fad83553f99bad3da07a838670850d36778
Artem Bondar <githublab13@gmail.com>
2025-01-01 16:19:00 -0500

    Refactor adjustTime function to improve readability and simplify logic for handling AM/PM

--- components/local-time/local-time.js
+++ components/local-time/local-time.js
@@ -5,4 +5,25 @@ export function localtime() {
     const secondsElement = document.querySelector('.local-time__clock--seconds');
     const periodElement = document.querySelector('.local-time__clock--period');
     const now = new Date();
+
+    function adjustTime() {
+        const hours = now.getHours();
+        if (hours >= 12) {
+            if (periodElement.classList.contains('local-time__clock--period--am')) {
+                periodElement.classList.remove('local-time__clock--period--am')
+            };
+            periodElement.classList.add('local-time__clock--period--pm');
+            periodElement.textContent = 'PM';
+
+            return String(hours > 12 ? hours - 12 : 12);
+        } else if (`${now.getHours()}` < 12) {
+            if (periodElement.classList.contains('local-time__clock--period--pm')) {
+                periodElement.classList.remove('local-time__clock--period--pm')
+            };
+            periodElement.classList.add('local-time__clock--period--am');
+            periodElement.textContent = 'AM';
+
+            return String(hours);
+        };
+    };
 };
