138716a4c34f73ca1becc805909d602d3250a008
Artem Bondar <githublab13@gmail.com>
2025-01-01 14:14:54 -0500

    Update path for searchMonthList import in change__month--suggestion.js

--- assets/script/mainScript.js
+++ assets/script/mainScript.js
@@ -1,6 +1,6 @@
 import { CreateCalendar } from '../../components/calendar/calendar.js';
 import { activateAllListeners } from '../../components/change/change.js';
-import { searchMonthList } from '../../components/input/input__month-search/input__month-search.js';
+import { searchMonthList } from '../../components/change/change__month--suggestion.js';
 
 const calendarCalendar = new CreateCalendar();
 calendarCalendar.currentCalendar();
