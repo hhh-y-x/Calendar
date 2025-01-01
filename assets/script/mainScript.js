4de8fb8f6e76844fc41b0e177c9cb0add147272b
Artem Bondar <githublab13@gmail.com>
2025-01-01 16:22:47 -0500

    Import and call localtime function for time section

--- assets/script/mainScript.js
+++ assets/script/mainScript.js
@@ -1,9 +1,11 @@
 import { CreateCalendar } from '../../components/calendar/calendar.js';
 import { activateAllListeners } from '../../components/change/change.js';
 import { searchMonthList } from '../../components/change/change__month--suggestion.js';
+import { localtime } from '../../components/local-time/local-time.js';
 
 const calendarCalendar = new CreateCalendar();
 calendarCalendar.currentCalendar();
 
 activateAllListeners();
 searchMonthList();
+localtime();
